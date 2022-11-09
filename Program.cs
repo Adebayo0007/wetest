6bdf1e05de7379e78aaa3d65255601e4b7399517
unknown <tijaniadebayoabdllahi@gmail.com>
2022-11-09 03:26:11 -0800

    this the first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gittestin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // This  is the beggining
+
+        }
+    }
+}
